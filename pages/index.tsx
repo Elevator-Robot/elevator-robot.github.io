29cbd709759029e45d545472c98a0c8a7e593599
John Weland <jweland@fluentstream.com>
2021-06-29 15:38:59 -0500

    build test

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,10 @@ const Home = () => {
       <Head>
         <title>elevator-robot.github.io</title>
         <meta name="description" content="elevator-robot.github.io" />
+        <link
+          href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
+          rel="stylesheet"
+        ></link>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>HelloWorld</h1>
